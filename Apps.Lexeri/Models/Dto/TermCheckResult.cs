ea80a509884a552691d81fa1a1fe4aebab3e6f92
sr189 <stefan.rohde@toptranslation.com>
2024-10-14 20:24:36 +0200

    style: add missing display attribute

--- Apps.Lexeri/Models/Dto/TermCheckResult.cs
+++ Apps.Lexeri/Models/Dto/TermCheckResult.cs
@@ -5,5 +5,7 @@ public class TermCheckResult
 {
     [Display("Includes forbidden terms", Description = "Wether the text includes forbidden terms")]
     public bool HasForbiddenTerms { get; set; }
+
+    [Display("Term matches", Description = "List of term matches found in the text")]
     public List<TermMatch>? TermMatches { get; set; }
 }
