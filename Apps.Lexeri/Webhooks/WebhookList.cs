c25c122e1d712ac1e236932c691822ce580a1b72
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-10-16 11:07:47 +0200

    Updated webhooks

--- Apps.Lexeri/Webhooks/WebhookList.cs
+++ Apps.Lexeri/Webhooks/WebhookList.cs
@@ -3,6 +3,7 @@ using Blackbird.Applications.Sdk.Common.Webhooks;
 using Newtonsoft.Json;
 using Apps.Lexeri.Models.Response;
 using Apps.Lexeri.Webhooks.Handlers;
+using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.Lexeri.Webhooks;
 
@@ -12,28 +13,28 @@ public class WebhookList
     #region Webhooks
 
     [Webhook("On term created", typeof(TermCreatedWebhookHandler), Description = "Triggered when a term was created")]
-    public Task<WebhookResponse<WebhookPayload>> OnTermCreated(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnTermCreated(WebhookRequest webhookRequest, [WebhookParameter][Display("Term ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     [Webhook("On term request created", typeof(TermRequestCreatedWebhookHandler), Description = "Triggered when a term request was created")]
-    public Task<WebhookResponse<WebhookPayload>> OnTermRequestCreated(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnTermRequestCreated(WebhookRequest webhookRequest, [WebhookParameter][Display("Term request ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     [Webhook("On term request finished", typeof(TermRequestFinishedWebhookHandler), Description = "Triggered when processing of a term request was finished by the terminology managers and its draft terms were published")]
-    public Task<WebhookResponse<WebhookPayload>> OnTermRequestFinished(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnTermRequestFinished(WebhookRequest webhookRequest, [WebhookParameter][Display("Term request ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     [Webhook("On import finished", typeof(ImportFinishedWebhookHandler), Description = "Triggered when processing of an import was finished by the terminology managers and its draft terms were published")]
-    public Task<WebhookResponse<WebhookPayload>> OnImportFinished(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnImportFinished(WebhookRequest webhookRequest, [WebhookParameter][Display("Import ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     [Webhook("On task finished", typeof(TaskFinishedWebhookHandler), Description = "Triggered when processing of a task was finished by the terminology managers and its draft terms were published")]
-    public Task<WebhookResponse<WebhookPayload>> OnTaskFinished(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnTaskFinished(WebhookRequest webhookRequest, [WebhookParameter][Display("Task ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     [Webhook("On term extraction finished", typeof(TermExtractionFinishedWebhookHandler), Description = "Triggered when processing of a term extraction was finished by the terminology managers and its draft terms were published")]
-    public Task<WebhookResponse<WebhookPayload>> OnTermExtractionFinished(WebhookRequest webhookRequest, [WebhookParameter] string? ID)
-        => HandleCallback<WebhookPayload>(webhookRequest);
+    public Task<WebhookResponse<WebhookPayload>> OnTermExtractionFinished(WebhookRequest webhookRequest, [WebhookParameter][Display("Term extraction ID")] string? identifier)
+        => HandleCallback(webhookRequest, identifier);
 
     // [Webhook("Check finished", typeof(CheckFinishedWebhookHandler), Description = "Triggered when a term check of a document is finished. Returns statistics about terms that have been found.")]
     // public Task<WebhookResponse<CheckFinishedWebhookPayload>> OnCheckFinished(WebhookRequest webhookRequest)
@@ -43,16 +44,26 @@ public class WebhookList
 
     #region Utils
 
-    private Task<WebhookResponse<T>> HandleCallback<T>(WebhookRequest request) where T : class
+    private async Task<WebhookResponse<WebhookPayload>> HandleCallback(WebhookRequest request, string? identifier)
     {
-        var payload = JsonConvert.DeserializeObject<T>(request.Body.ToString(),
+        var payload = JsonConvert.DeserializeObject<WebhookPayload>(request.Body.ToString(),
             new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Ignore });
-        
-        return Task.FromResult(new WebhookResponse<T>
+
+        if (identifier != null && payload.Identifier != identifier)
+        {
+            return new()
+            {
+                HttpResponseMessage = null,
+                Result = null,
+                ReceivedWebhookRequestType = WebhookRequestType.Preflight
+            };
+        }
+
+        return new WebhookResponse<WebhookPayload>
         {
             HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
             Result = payload
-        });
+        };
     }
 
     #endregion
